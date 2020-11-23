0ddbf769dbae98a8f9a4de3b413434858a9d4ff0
Moris Kotsch <moriskotsch@gmail.com>
2020-11-23 19:57:50 +0100

    add php website

---
+++ CompressorOnOff/Compressor.py
@@ -0,0 +1,14 @@
+import paho.mqtt.client as mqtt
+import sys
+    
+def on_publish(client, userdata, result):
+    print("data published \n")
+    pass
+
+BROKER_ADDRESS = "localhost"
+print("Connected to MQTT Broker: " + BROKER_ADDRESS)
+
+client = mqtt.Client()
+client.on_publish = on_publish
+client.connect(BROKER_ADDRESS)
+ret = client.publish("/mqtt/compressor",sys.argv[1])
