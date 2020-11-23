1cc11a51d35566617900967ad852da463e6c75e3
Moris Kotsch <moriskotsch@gmail.com>
2020-11-23 20:48:55 +0100

    beginn factoryController

---
+++ factoryWebsite/ProcessingStation.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>
+			Fischertechnik 4.0
+		</title>
+	</head>
+	<body>
+		<h2>
+			Zurück zur Übersicht!
+		</h2>
+		<input type="button" onclick="location.href='index.php';" value="Übersicht" />
+		<h4>
+			Bitte wählen Sie einen Bereich der Anlage aus!
+		</h4>
+		<form method="post">
+			<p>
+				<button name="buttonWhite">SortingLine</button>
+				<button name="buttonRed">ProcessingStation</button>
+				<button name="buttonBlue">MainUnit</button>
+			</p>
+		</form>
+	</body>
+</html>
---
+++ factoryWebsite/SortingLine.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>
+			Sorting Line
+		</title>
+	</head>
+	<body>
+		<h2>
+			Zurück zur Übersicht!
+		</h2>
+		<input type="button" onclick="location.href='index.php';" value="Übersicht" />
+		<h4>
+			Steuerung der Schieber!
+		</h4>
+		<form method="post">
+			<p>
+				<button name="buttonWhite">Weiß</button>
+				<button name="buttonRed">Rot</button>
+				<button name="buttonBlue">Blau</button>
+			</p>
+		</form>
+	</body>
+</html>
