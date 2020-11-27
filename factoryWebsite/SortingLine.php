ba7285602a6c465d6f74302c4b00a46e80e1e1be
Moris Kotsch <moriskotsch@gmail.com>
2020-11-27 23:59:58 +0100

    optimized SortingLine.php

--- factoryWebsite/SortingLine.php
+++ factoryWebsite/SortingLine.php
@@ -10,6 +10,14 @@
 		</h2>
 		<input type="button" onclick="location.href='index.php';" value="Übersicht" />
 		<h4>
+			Steuerung der Druckluft!
+		</h4>
+		<form method="post">
+			<p>
+				<button name="buttonCompressor">Druckluft</button>
+			</p>
+		</form>
+		<h4>
 			Steuerung der Schieber!
 		</h4>
 		<form method="post">
@@ -21,3 +29,30 @@
 		</form>
 	</body>
 </html>
+
+<?php
+	if (isset($_POST['buttonCompressor']))
+	{
+		$text = "AirCompressor";
+		$cmd = 'sudo python /var/www/html/CompressorOnOff/Compressor.py "'.$text.'"';
+		shell_exec($cmd);
+	}
+	if (isset($_POST['buttonWhite']))
+	{
+		$text = "White";
+		$cmd = 'sudo python /var/www/html/CompressorOnOff/Compressor.py "'.$text.'"';
+		shell_exec($cmd);
+	}
+	if (isset($_POST['buttonRed']))
+	{
+		$text = "Red";
+		$cmd = 'sudo python /var/www/html/CompressorOnOff/Compressor.py "'.$text.'"';
+		shell_exec($cmd);
+	}
+	if (isset($_POST['buttonBlue']))
+	{
+		$text = "Blue";
+		$cmd = 'sudo python /var/www/html/CompressorOnOff/Compressor.py "'.$text.'"';
+		shell_exec($cmd);
+	}
+?>
