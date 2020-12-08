9965a8a7d3168a8ed491a1451e274871f90d48ea
Moris Kotsch <moriskotsch@gmail.com>
2020-12-08 22:48:31 +0100

    test database with demo

--- factoryWebsite/demo/database connection.php
+++ factoryWebsite/demo/database connection.php
@@ -1,4 +1,4 @@
 <?php
-$connection = new PDO( 'mysql:host=localhost;dbname=course_db', 'root', '' );
+$connection = new PDO( 'mysql:host=localhost;dbname=FromWebToFactory', 'raspberry', 'ftpiuser' );
 ?>
                            
