f6e2635a5ef846dbefd030948e46952f8a172318
Moris Kotsch <moriskotsch@gmail.com>
2021-03-30 21:09:05 +0200

    remove old mqtt_statements

--- ControllableFactory/websiteFactory/src/WebsiteMainUnit.cpp
+++ ControllableFactory/websiteFactory/src/WebsiteMainUnit.cpp
@@ -127,13 +127,11 @@ int main()
 }
 
 void processOrder(){
-    if (warehouse.storage.getQuantityOf(orderColor) > 0)
-    {
-        /* code */
-    }else
+    if (warehouse.storage.getQuantityOf(orderColor) == 0)
     {
         checkAvailableWorkpieceOfColor();
     }
+    getWorkpieceWithColor();
 }
 
 void checkAvailableWorkpieceOfColor(){
@@ -189,7 +187,7 @@ void checkAvailableWorkpieceOfColor(){
 }
 
 void getWorkpieceWithColor(){
-    // TODO move vaccuum robot to warehouse
+    //Logic
     getFullBox();
     driveBeltTo(BeltState::VACUUM_ROBOT);
     std::thread processing = std::thread(driveToProcessing);
@@ -201,70 +199,8 @@ void getWorkpieceWithColor(){
     processing.join();
 }
 
-/*
-void checkAvailableWorkpieces()
-{
-    if (!white_available.value())
-    {
-        std::thread wait = std::thread([] {
-            while (warehouse.state != HighBayState::H_READY)
-            {
-                sleep(10ms);
-            }
-            getEmptyBox(Color::WHITE);
-            driveBeltTo(BeltState::VACUUM_ROBOT);
-        });
-        driveToWarehouse(Color::WHITE);
-        wait.join();
-        storeBox(WarehouseContent::WHITE);
-    }
-    else if (!red_available.value())
-    {
-        std::thread wait = std::thread([] {
-            while (warehouse.state != HighBayState::H_READY)
-            {
-                sleep(10ms);
-            }
-            getEmptyBox(Color::RED);
-            driveBeltTo(BeltState::VACUUM_ROBOT);
-        });
-        driveToWarehouse(Color::RED);
-        wait.join();
-        storeBox(WarehouseContent::RED);
-    }
-    else if (!blue_available.value())
-    {
-        std::thread wait = std::thread([] {
-            while (warehouse.state != HighBayState::H_READY)
-            {
-                sleep(10ms);
-            }
-            getEmptyBox(Color::BLUE);
-            driveBeltTo(BeltState::VACUUM_ROBOT);
-        });
-        driveToWarehouse(Color::BLUE);
-        wait.join();
-        storeBox(WarehouseContent::BLUE);
-    }
-    else
-    {
-        // TODO move vaccuum robot to warehouse
-        getFullBox();
-        driveBeltTo(BeltState::VACUUM_ROBOT);
-        std::thread processing = std::thread(driveToProcessing);
-        while (beltstate != BeltState::WAREHOUSE)
-        {
-            sleep(10ms);
-        }
-        storeBox(WarehouseContent::EMPTY_BOX);
-        processing.join();
-    }
-}
-*/
-
 void driveToWarehouse(Color color)
 {
-    mqttClient->publishMessageAsync(TOPIC_INPUT_VACUUMROBOT_STATE, "holen", DFLT_QUALITY_OF_SERVICE, true);
     robot.yaxis.moveAbsolut(0);
     if (color == Color::WHITE)
     {
@@ -289,12 +225,10 @@ void driveToWarehouse(Color color)
     robot.yaxis.moveAbsolut(WAREHOUSE_Y);
     robot.release();
     robot.yaxis.moveAbsolut(0);
-    //mqttClient->publishMessageAsync(TOPIC_INPUT_VACUUMROBOT_STATE, "bereit", DFLT_QUALITY_OF_SERVICE, true);
 }
 
 void driveToProcessing()
 {
-    //mqttClient->publishMessageAsync(TOPIC_INPUT_VACUUMROBOT_STATE, "abliefern", DFLT_QUALITY_OF_SERVICE, true);
     robot.yaxis.moveAbsolut(0);
     robot.drive(WAREHOUSE_X, WAREHOUSE_Y, WAREHOUSE_Z);
     robot.suck();
@@ -308,12 +242,10 @@ void driveToProcessing()
     robot.zaxis.moveAbsolut(0);
     beltState.join();
     xaxis.join();
-    //mqttClient->publishMessageAsync(TOPIC_INPUT_VACUUMROBOT_STATE, "bereit", DFLT_QUALITY_OF_SERVICE, true);
 }
 
 void storeWorkpieceHighBay(uint8_t x, uint8_t y, WarehouseContent content)
 {
-    //mqttClient->publishMessageAsync(TOPIC_INPUT_WAREHOUSE_STATE, "einlagern", DFLT_QUALITY_OF_SERVICE, true);
     warehouse.state = HighBayState::H_STORE_WORKPIECE;
     std::thread beltThread = driveBeltToAsync(BeltState::WAREHOUSE);
     warehouse.drive(3, 3);
@@ -323,10 +255,7 @@ void storeWorkpieceHighBay(uint8_t x, uint8_t y, WarehouseContent content)
     warehouse.put();
     warehouse.storage.setWorkpieceAt(y * 3 + x, content);
 
-    //mqttClient->publishMessageAsync(TOPIC_INPUT_STOCK, warehouse.storage.getAsJson(), DFLT_QUALITY_OF_SERVICE, true);
-
     warehouse.state = HighBayState::H_READY;
-    //mqttClient->publishMessageAsync(TOPIC_INPUT_WAREHOUSE_STATE, "bereit", DFLT_QUALITY_OF_SERVICE, true);
 }
 
 void getEmptyBox(Color color)
@@ -353,18 +282,12 @@ void storeBox(WarehouseContent content)
 
 void getWorkpieceHighBay(uint8_t x, uint8_t y)
 {
-    //mqttClient->publishMessageAsync(TOPIC_INPUT_WAREHOUSE_STATE, "auslagern", DFLT_QUALITY_OF_SERVICE, true);
     warehouse.state = HighBayState::H_PROVIDE_WORKPIECE;
     warehouse.drive(x, y);
     warehouse.pull();
-    warehouse.storage.setWorkpieceAt(y * 3 + x, WarehouseContent::NO_BOX);
-
-    //mqttClient->publishMessageAsync(TOPIC_INPUT_STOCK, warehouse.storage.getAsJson(), DFLT_QUALITY_OF_SERVICE, true);
-
-    warehouse.drive(3, 3);
+    warehouse.storage.setWorkpieceAt(y * 3 + x, WarehouseContent::NO_BOX);warehouse.drive(3, 3);
     warehouse.put(true);
     warehouse.state = HighBayState::H_READY;
-    //mqttClient->publishMessageAsync(TOPIC_INPUT_WAREHOUSE_STATE, "bereit", DFLT_QUALITY_OF_SERVICE, true);
 }
 
 void getFullBox()
@@ -395,7 +318,6 @@ void driveBeltTo(BeltState state) {
     {
         belt.right(450);
         light_sensor_vacuum_robot.waitFor(DigitalState::LOW);
-        // drive a bit more than the light sensor
         sleep(100ms);
         belt.stop();
         beltstate = BeltState::VACUUM_ROBOT;
